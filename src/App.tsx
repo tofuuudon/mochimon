2e3ed3ac24068fce4a7db22bf7bc451cd1aa0fb5
M Lau <63963445+tofuuudon@users.noreply.github.com>
2025-02-17 20:55:26 +0000

    feat: add notifications plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-export default function App() {
+import {
+  isPermissionGranted,
+  requestPermission,
+  sendNotification,
+} from "@tauri-apps/plugin-notification";
+import "./titlebar.css";
+
+function App() {
+  async function sayHello() {
+    let permissionGranted = await isPermissionGranted();
+
+    if (!permissionGranted) {
+      const permission = await requestPermission();
+      permissionGranted = permission === "granted";
+    }
+
+    if (permissionGranted) {
+      sendNotification({ title: "Tauri", body: "Tauri is awesome!" });
+    }
+  }
+
   return (
-    <main>
+    <main className="container">
       <h1>Welcome to Mochimon</h1>
+      <button onClick={sayHello}>Press Me</button>
     </main>
   );
 }
+
+export default App;
